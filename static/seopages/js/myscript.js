407584f3614e35e93726bea148b71be73897f849
BharathGrocat <bharathkumar@grocat.in>
2026-02-24 10:29:37 +0530

    Update myscript.js

--- static/seopages/js/myscript.js
+++ static/seopages/js/myscript.js
@@ -124,6 +124,35 @@ $(".meet_our_div").owlCarousel({
   }
 });
 
+$(".service-carousel").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: true,
+  autoHeight: true,
+  dots: true,
+  navText: false,
+  autoplayHoverPause: true,
+  navText: [
+    '<i class="fa-solid fa-arrow-left"></i>',
+    '<i class="fa-solid fa-arrow-right"></i>'
+  ],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    900: {
+      items: 3
+    },
+    1200: {
+      items: 4,
+      loop: false
+    }
+  }
+});
+
 $(document).ready(function () {
   $(".over_lay_div").owlCarousel({
     loop: true,
@@ -138,8 +167,8 @@ $(document).ready(function () {
     responsive: {
       0: { items: 1 },
       600: { items: 2 },
-      990: { items: 3 },
-      1250: { items: 3.5 }
+      1250: { items: 2.5 },
+      1300: { items: 3.5 },
     }
   });
 });
@@ -162,11 +191,13 @@ $(document).ready(function () {
     responsive: {
       0: { items: 1 },
       600: { items: 2 },
-      767: { items: 3 },
+      900: { items: 3 },
       1300: { items: 4 }
     }
   });
 });
+
+
 
 $(document).ready(function () {
   $(".journey-steps").owlCarousel({
